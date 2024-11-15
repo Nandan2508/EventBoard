52c7776d89d37a167c5de64f62df052630a0d908
krishan2005op <krishanmalhotra2005@gmail.com>
2024-11-15 21:54:22 +0530

    hi

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
+// eslint-disable-next-line no-unused-vars
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Student from './student';  // or wherever your Student component is located
+// import Student from './student';  // or wherever your Student component is located
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Student/>} />
-      </Routes>
-    </Router>
+    // <Router>
+    //   <Routes>
+    //     <Route path="/" element={<Student/>} />
+    //   </Routes>
+    // </Router>
+    <></>
   );
 };
 
